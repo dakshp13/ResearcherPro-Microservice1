c7f7ade4faf20aa0e0ef9122dbf137f20003dbb0
Daksh Patel <dakshpatel1700@gmail.com>
2025-07-17 18:01:45 -0400

    Major UI and Controller Changes

--- microservice1/src/main/java/com/researcherpro/microservice1/Controller/MainController.java
+++ microservice1/src/main/java/com/researcherpro/microservice1/Controller/MainController.java
@@ -3,6 +3,7 @@ package com.researcherpro.microservice1.Controller;
 import com.researcherpro.microservice1.Model.LoginCreds;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,6 +15,23 @@ public class MainController {
     @GetMapping("/login")
     public String loginPage(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginAuth(@ModelAttribute LoginCreds loginCreds, Model model){
+        if(loginCreds.getUsername().equals("guest123")
+        && loginCreds.getPassword().equals("guest123")){
+            return "redirect:/api/analytics";
+        }
+        else{
+            model.addAttribute("error", "Failed Login");
+            return "login";
+        }
+    }
+
+    @GetMapping("/analytics")
+    public String analyticsPage(){
+        return "analytics";
     }
 
 }
